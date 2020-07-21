24a33899b4598753ed48d60082a483ae2914eed9
afizihusaini <afizihusaini@pipeline-network.com>
2020-07-21 11:41:05 +0800

    Add baseURL

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  baseUrl: "https://caam-sbs-api.pipe.my/",
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYWZlZXpheml6IiwiYSI6ImNqNjJ6anlhYzA0bTczM3FvYnppbDh4eTEifQ.AdDRr42bNfNJvQENLrE6eg' // Your access token goes here
   }
