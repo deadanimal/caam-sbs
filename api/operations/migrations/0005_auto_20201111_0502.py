cdba47736c45dd8e17d01ffee089678b1f980b9c
farrahzelyna <farrahzelyna@gmail.com>
2020-11-12 20:03:06 +0800

    fix operation migration

--- api/operations/migrations/0005_auto_20201111_0502.py
+++ api/operations/migrations/0005_auto_20201111_0502.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('operations', '0004_auto_20201018_0550'),
+        ('operations', '0002_auto_20201112_0825'),
     ]
 
     operations = [
